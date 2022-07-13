966d41da21d35efb284cd798fa9d43f42ae07fa3
mukul <jmukul00@gmail.com>
2022-07-13 16:42:08 +0530

    Interest Activty Completed

--- app/src/main/java/com/example/wallpaperapp/adapter/InterestAdapter.kt
+++ app/src/main/java/com/example/wallpaperapp/adapter/InterestAdapter.kt
@@ -2,39 +2,53 @@ package com.example.wallpaperapp.adapter
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.models.InterestModel
+import com.example.wallpaperapp.ui.interest.InterestActivity
 
-class InterestAdapter(val context:Context, val interestList: List<InterestModel>): RecyclerView.Adapter<InterestAdapter.MyViewHolder>() {
+class InterestAdapter(
+    val context: Context,
+    val interestList: List<InterestModel>,
+    val interestSelectedListener: InterestSelectedListener
+) : RecyclerView.Adapter<InterestAdapter.MyViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_interest, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_interest, parent, false)
         return InterestAdapter.MyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val interestModel = interestList[position]
         Log.d("InterestAdapter", "onDataChange: " + interestModel.interestName)
-        holder.txtInterestName.text =  interestModel.interestName
+        holder.txtInterestName.text = interestModel.interestName
         holder.itemView.setOnClickListener {
-            interestModel.isSelected = !interestModel.isSelected
 
-            if (interestModel.isSelected){
-                holder.txtInterestName.setBackgroundResource(R.drawable.tab_color)
-                holder.txtInterestName.setTextColor(ContextCompat.getColor(context, R.color.white))
+                interestModel.isSelected = !interestModel.isSelected
+            if (getSelected().size>6) {
+                Toast.makeText(context, "Can select only 6 interests", Toast.LENGTH_SHORT).show()
+                interestModel.isSelected = !interestModel.isSelected
             }
-            else{
+
+            if (interestModel.isSelected) {
+                    holder.txtInterestName.setBackgroundResource(R.drawable.tab_color)
+                    holder.txtInterestName.setTextColor(ContextCompat.getColor(context, R.color.white))
+                    interestSelectedListener.onInterestSelected()
+            } else {
                 with(holder) {
                     txtInterestName.setBackgroundResource(R.drawable.tab_bg)
                     txtInterestName.setTextColor(ContextCompat.getColor(context, R.color.primary))
+                    interestSelectedListener.onInterestSelected()
                 }
             }
         }
@@ -44,18 +58,25 @@ class InterestAdapter(val context:Context, val interestList: List<InterestModel>
         return interestList.size
     }
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val txtInterestName = itemView.findViewById<TextView>(R.id.txt_interest_name)
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val txtInterestName: TextView = itemView.findViewById(R.id.txt_interest_name)
     }
 
-    public fun getSelected(): ArrayList<String>{
-        val selected:ArrayList<String> = ArrayList<String>()
-        for(i in interestList.indices){
+    fun getSelected(): ArrayList<String> {
+        val selected: ArrayList<String> = ArrayList()
+        for (i in interestList.indices) {
             Log.d("TAG", "getSelected: " + (interestList[i].isSelected))
             if (interestList[i].isSelected) {
                 selected.add(interestList[i].interestName!!)
             }
         }
+
+
         return selected
     }
+}
+
+
+interface InterestSelectedListener {
+    fun onInterestSelected()
 }
--- app/src/main/java/com/example/wallpaperapp/ui/interest/InterestActivity.kt
+++ app/src/main/java/com/example/wallpaperapp/ui/interest/InterestActivity.kt
@@ -1,10 +1,12 @@
 package com.example.wallpaperapp.ui.interest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.GridLayout
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -13,46 +15,50 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.wallpaperapp.R
 import com.example.wallpaperapp.adapter.InterestAdapter
+import com.example.wallpaperapp.adapter.InterestSelectedListener
 import com.example.wallpaperapp.databinding.ActivityInterestBinding
 import com.example.wallpaperapp.models.InterestModel
 import com.example.wallpaperapp.ui.ui.home.HomeViewModel
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.core.Context
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlin.math.ceil
 import kotlin.math.roundToInt
 
-class InterestActivity : AppCompatActivity() {
+class InterestActivity : AppCompatActivity(), InterestSelectedListener {
     private val TAG = "InterestActivity"
-    val interestList: ArrayList<InterestModel> = ArrayList()
     lateinit var interestAdapter: InterestAdapter
     private lateinit var interestViewModel: InterestViewModel
+    lateinit var interestBinding: ActivityInterestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityInterestBinding.inflate(layoutInflater)
+        interestBinding = ActivityInterestBinding.inflate(layoutInflater)
+        Log.d(TAG, "onCreate:" + interestBinding)
         interestViewModel = ViewModelProvider(this)[InterestViewModel::class.java]
-
 
 
         interestViewModel.getInterestMutableLiveData().observe(this) {
 
             Log.d(TAG, "onCreate: $it")
-            interestAdapter = InterestAdapter(applicationContext, it)
-            var spanCount = 0;
-            spanCount = if (it.size<=5) {
+            interestAdapter = InterestAdapter(applicationContext, it, this)
+            val spanCount: Int = if (it.size<=5) {
                 2
             } else {
                 ceil((it.size.toDouble()/3.0)).roundToInt()
             }
             val layoutManager = StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.HORIZONTAL)
-            binding.rvInterest.layoutManager = layoutManager
-            binding.rvInterest.adapter = interestAdapter
+            interestBinding.rvInterest.layoutManager = layoutManager
+            interestBinding.rvInterest.adapter = interestAdapter
+
 
         }
 
-        binding.btnExplore.setOnClickListener {
+
+
+        interestBinding.btnExplore.setOnClickListener {
            if(interestAdapter.getSelected().size in 3..6){
 
            }
@@ -61,6 +67,20 @@ class InterestActivity : AppCompatActivity() {
            }
         }
 
-        setContentView(binding.root)
+        setContentView(interestBinding.root)
     }
+
+    override fun onInterestSelected() {
+        if (interestAdapter.getSelected().size in 3..6){
+            interestBinding.btnExplore.setBackgroundColor(ContextCompat.getColor(this, R.color.primary))
+            interestBinding.btnExplore.isClickable = true
+        }
+        else{
+            interestBinding.btnExplore.setBackgroundColor(ContextCompat.getColor(this, R.color.gray))
+            interestBinding.btnExplore.isClickable = false
+        }
+
+
+    }
+
 }
